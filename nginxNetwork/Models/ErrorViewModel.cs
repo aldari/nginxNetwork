d41efc4f9833bb8cfab3ab4924485d72959caf41
El'dar Ismagilov <eldarismagilov@gmail.com>
2019-04-10 20:05:36 +0500

    ASP Core default template setup with nginx

---
+++ nginxNetwork/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace nginxNetwork.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
